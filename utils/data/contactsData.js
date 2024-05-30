9d8e0ffb01c81b26d55002555b44889179e7dcb5
Vikrantbais42 <vikrantbais70@gmail.com>
2024-05-31 01:15:29 +0530

    UiChnages

--- utils/data/contactsData.js
+++ utils/data/contactsData.js
@@ -1,11 +1,11 @@
 export const contactsData = {
-    email: 'abusaid7388@gmail.com',
-    phone: '+8801608797655',
-    address: 'Middle Badda, Dhaka, Bangladesh - 1212 ',
-    github: 'https://github.com/said7388',
-    facebook: 'https://www.facebook.com/abusaid.riyaz/',
-    linkedIn: 'https://www.linkedin.com/in/abu-said-bd/',
-    twitter: 'https://twitter.com/said7388',
-    stackOverflow: 'https://stackoverflow.com/users/16840768/abu-said',
-    devUsername: "said7388"
+    email: 'vikrants805@gmail.com',
+    phone: '+917011615578',
+    address: 'Golden Nest Society, B-Block, Sector-62, Noida',
+    github: 'https://www.linkedin.com/in/vikrant-bais-ba6a86129/',
+    facebook: 'https://www.linkedin.com/in/vikrant-bais-ba6a86129/',
+    linkedIn: 'https://www.linkedin.com/in/vikrant-bais-ba6a86129/',
+    twitter: 'https://www.linkedin.com/in/vikrant-bais-ba6a86129/',
+    stackOverflow: 'https://www.linkedin.com/in/vikrant-bais-ba6a86129/',
+    devUsername: ""
 }
--- utils/data/personal-data.js
+++ utils/data/personal-data.js
@@ -5,13 +5,13 @@ export const personalData = {
   description: "With over 3+ years of experience, I'm a skilled software engineer adept at designing and developing robust, scalable, and high-performance software solutions. My expertise spans various programming languages, software development methodologies, and tools, enabling me to tackle diverse challenges effectively. I am deeply passionate about innovation, problem-solving, and delivering exceptional user experiences. Furthermore, I possess strong communication and collaboration skills, with a proven ability to thrive in team environments.",
   email: 'vikrantbais70@gmail.com',
   phone: '+917011615678',
-  address: 'Golden Nest Society B-Block Sector 62 Noida ',
+  address: 'Golden Nest Society, B-Block, Sector-62, Noida',
   github: 'https://www.linkedin.com/in/vikrant-bais-ba6a86129/',
   facebook: 'https://www.linkedin.com/in/vikrant-bais-ba6a86129/',
   linkedIn: 'https://www.linkedin.com/in/vikrant-bais-ba6a86129/',
   twitter: 'https://www.linkedin.com/in/vikrant-bais-ba6a86129/',
   stackOverflow: 'https://www.linkedin.com/in/vikrant-bais-ba6a86129/',
   leetcode: "https://www.linkedin.com/in/vikrant-bais-ba6a86129/",
-  devUsername: "said7388",
-  resume: "https://drive.google.com/file/d/1eyutpKFFhJ9X-qpQGKhUNnVRkB5Wer00/view?usp=sharing"
+  devUsername: "",
+  resume: "https://docs.google.com/document/d/1EJnKwKtb0cE4PM1X_6ABYAxGr6LPsdwdCf3-wGUn64k/edit?usp=sharing"
 }
