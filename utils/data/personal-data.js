737292c2a81f384a6cea2bc2ca49dc7de6fb2cd9
Vikrantbais42 <vikrantbais70@gmail.com>
2024-05-31 01:28:18 +0530

    resume

--- utils/data/personal-data.js
+++ utils/data/personal-data.js
@@ -13,5 +13,5 @@ export const personalData = {
   stackOverflow: 'https://www.linkedin.com/in/vikrant-bais-ba6a86129/',
   leetcode: "https://www.linkedin.com/in/vikrant-bais-ba6a86129/",
   devUsername: "",
-  resume: "https://docs.google.com/document/d/1EJnKwKtb0cE4PM1X_6ABYAxGr6LPsdwdCf3-wGUn64k/edit?usp=sharing"
+  resume: "https://drive.google.com/file/d/16NfeDn6oTMQz5M10tsJDChllOlHVRjCo/view?usp=sharing"
 }
